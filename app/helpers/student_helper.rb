c2960db0b299f3d14628849d6ebe5b7093ae03f1
free2580 <fridakahsas@yahoo.com>
2018-10-14 18:00:18 -0400

    Done.

---
+++ app/helpers/student_helper.rb
@@ -0,0 +1,4 @@
+module StudentHelper
+
+
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Sammy", last_name: "Casas")
+Student.create(first_name: "Joplin", last_name: "Casas")
+Student.create(first_name: "Sadie", last_name: "Casas")
+Student.create(first_name: "Marilyn", last_name: "Casas")
+
+SchoolClass.new(title: "Data Science", room_number: 435)
+SchoolClass.new(title: "Science", room_number: 444)
+SchoolClass.new(title: "History", room_number: 335)
