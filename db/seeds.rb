22ce3e76c23c8162bf030c5283432f9e7f5685c9
srscalia <shelby.scalia@gmail.com>
2018-10-15 17:25:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create(first_name: "Shelby", last_name: "Trinca")
+Student.create(first_name: "Mason", last_name: "Trinca")
+Student.create(first_name: "Lindsay", last_name: "Trinca")
+Student.create(first_name: "Harrison", last_name: "Trinca")
+Student.create(first_name: "Jackson", last_name: "Trinca")
+
+
+SchoolClass.create(title: "History", room_number: 12)
+SchoolClass.create(title: "Science", room_number: 12)
+SchoolClass.create(title: "AP English", room_number: 12)
+SchoolClass.create(title: "PE", room_number: 12)
+SchoolClass.create(title: "Math", room_number: 12)
